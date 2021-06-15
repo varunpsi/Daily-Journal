fcf0a6e787a515f3b12d05a21d0d8f4acbd0050f
Varun Pratap Singh <varunpsi3004@gmail.com>
2021-06-15 21:48:37 +0530

    edited app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express, { static } from "express";
-import { urlencoded } from "body-parser";
-import ejs from "ejs";
-import lodash from "lodash";
-import { connect, model } from "mongoose";
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,13 +11,13 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(static(__dirname));
+app.use(express.static(__dirname));
 
-app.use(urlencoded({
+app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-connect("mongodb+srv://varunpsi:Test123@cluster0.tkij0.mongodb.net/blogDB", {
+mongoose.connect("mongodb+srv://varunpsi:Test123@cluster0.tkij0.mongodb.net/blogDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -27,7 +27,7 @@ const postSchema = {
   content: String
 };
 
-const Post = model("Post", postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
 
